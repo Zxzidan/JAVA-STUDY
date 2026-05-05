f749355b557fcb62769fbc8e6db7d4f66078c9c1
Zxidan <dandiazaidane05@gmail.com>
2026-05-05 10:15:53 +0700

    Assignments Operator

---
+++ assignmentsOperators.java
@@ -0,0 +1,8 @@
+public class assignmentsOperators {
+    public static void main(String[] args){
+        int savings = 1000;
+        savings += 500;
+
+        System.out.println(savings);
+    }
+}
