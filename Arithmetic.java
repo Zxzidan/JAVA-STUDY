a5b54c8d8765480c43ab29ecc91bd01476d8c7b3
Zxidan <dandiazaidane05@gmail.com>
2026-05-05 09:24:19 +0700

    update

--- Arithmetic.java
+++ Arithmetic.java
@@ -8,5 +8,11 @@ public class Arithmetic {
         System.out.println(a * b);
         System.out.println(a / b);
         System.out.println(a % b);
+
+        ++a;
+        System.out.println(a);
+
+        --b;
+        System.out.println(b);
     }
 }
